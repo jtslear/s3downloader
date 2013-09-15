79869d96ffb2efa32802948734f2dd7ff5ce4805
John T Skarbek <jtslear@gmail.com>
2013-09-15 00:51:47 -0400

    Improve the authentication mechanism, put it into it's own class.

--- bin/s3logdownloader.rb
+++ bin/s3logdownloader.rb
@@ -1,13 +1,29 @@
-#!?usr/bin/ruby
-
 require 'rubygems'
 require 'aws/s3'
 
-get_sum = AWS::S3.initialize
-get_sum.establish_connection!(
-      :access_key_id     => 'ENV[ACCESS_KEY_ID]', 
-      :secret_access_key => 'ENV[SECRET_ACCESS_KEY]',
-      :use_ssl           => true
-    )
 
-get_sum.Service.buckets
+class Connect
+  def initialize
+    @access_key_id     = ENV['ACCESS_KEY_ID']
+    @secret_access_key = ENV['SECRET_ACCESS_KEY']
+  end
+  
+  def authenticate
+    if !(@access_key_id||@secret_access_key)
+      print "Please specify the following ENV variables:
+        ACCESS_KEY_ID
+        SECRET_ACCESS_KEY\n"
+      exit
+    else
+      get_sum = AWS::S3::Base.establish_connection!(
+        :access_key_id     => @access_key_id, 
+        :secret_access_key => @secret_access_key,
+        :use_ssl           => true
+      )
+    end
+  end
+end
+
+#get_sum.connected?
+get_sum = Connect.new
+get_sum.authenticate
